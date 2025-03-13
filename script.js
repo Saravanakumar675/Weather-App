58a115df84e0ef26a8025c1409d8d1e3b34f31e0
Saravanakumar675 <126373605+Saravanakumar675@users.noreply.github.com>
2025-03-13 14:57:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const apiKey = "7ffeda6a84d6d91e5e21507c59a46417";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather"; 
 const URL = apiUrl +"appid=" + apiKey + "&q=" + "London" + "&units=metric";
 import images from 'images';
-
+import clear from "images/clear.png";
+import cloudy from "images/cloudy.png";
+import rain from "images/rain.png";
+import drizzle from "images/drizzle.png";
+import mist from "images/mist.png";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
@@ -32,19 +36,19 @@ async function checkWeather(city){
         document.querySelector(".wind").innerHTML = data.wind.speed + " km/h" ;
     
         if(data.weather[0].main =="Clouds"){
-            weatherIcon.src = "images/cloudy.png";
+            weatherIcon.src = {cloudy};
         }
         else if(data.weather[0].main =="Clear"){
-            weatherIcon.src = "images/clear.png";
+            weatherIcon.src = {clear};
         }
         else if(data.weather[0].main =="Rainr"){
-            weatherIcon.src = "images/Rain.png";
+            weatherIcon.src = {rain};
         }
         else if(data.weather[0].main =="Drizzle"){
-            weatherIcon.src = "images/Drizzle.png";
+            weatherIcon.src = {drizzle};
         }
         else if(data.weather[0].main =="Mist"){
-            weatherIcon.src = "images/mist.png";
+            weatherIcon.src = {mist};
         }
     
         document.querySelector(".Weather").style.display = "block";
